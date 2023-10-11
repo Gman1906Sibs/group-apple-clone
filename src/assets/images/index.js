b77e6601d23a942b1691e022a75b0995b3da4a87
Gman1906Sibs <77187437+Gman1906Sibs@users.noreply.github.com>
2023-10-11 21:00:32 +0200

    dummy data

---
+++ src/assets/images/index.js
@@ -0,0 +1,103 @@
+// Logo
+import logo from "./apple-logo.png";
+import whiteLogo from "./white-apple.png";
+import blackLogo from "./black-apple.png";
+
+// home screen
+import home14Banner from "./i4ProBanner.jpg";
+import homeIphone14 from "./iphone-14.png";
+import homeIpad from "./ipad_home.png";
+import homeIwatch from "./iwatch_8.png";
+import homeM2 from "./macbookM2.png";
+import homeStudio from "./studio.png";
+import homeWatchDeal from "./watch_deal.webp";
+
+// home screen *mobile device*
+import mobileHome from "./mobileHome.jpg";
+
+// mac
+import imac1 from "./imac1.png";
+import imac2 from "./imac2.png";
+import mac1 from "./mac1.png";
+import mac2 from "./mac2.png";
+import mac3 from "./mac3.png";
+import studio1 from "./studio.png";
+import studio2 from "./studio2.png";
+
+// ipad
+import ipad1 from "./ipad1.png";
+import ipad2 from "./ipad2.png";
+import ipad3 from "./ipad3.png";
+import ipad4 from "./ipad4.png";
+import ipad5 from "./ipad5.png";
+
+// iphone
+import iphone1 from "./iphone1.png";
+import iphone2 from "./iphone2.png";
+import iphone3 from "./iphone3.png";
+import iphone4 from "./iphone4.png";
+import iphone5 from "./iphone5.png";
+
+// watch
+import iwatch1 from "./watch.png";
+import iwatch2 from "./watch1.png";
+import iwatch3 from "./watch2.png";
+import iwatch4 from "./watch3.png";
+
+// shipping
+import aramex from "./aramex.png"
+import courier from "./courier.png"
+import pep from "./pep.jpeg"
+
+export {
+    // logo
+    logo,
+    whiteLogo,
+    blackLogo,
+
+    // home screen
+    home14Banner,
+    homeIpad,
+    homeIphone14,
+    homeIwatch,
+    homeM2,
+    homeStudio,
+    homeWatchDeal,
+
+    // home screen *mobile device*
+    mobileHome,
+
+    //mac
+    imac1,
+    imac2,
+    mac1,
+    mac2,
+    mac3,
+    studio1,
+    studio2,
+
+    // ipad
+    ipad1,
+    ipad2,
+    ipad3,
+    ipad4,
+    ipad5,
+
+    // iphone
+    iphone1,
+    iphone2,
+    iphone3,
+    iphone4,
+    iphone5,
+
+    // watch
+    iwatch1,
+    iwatch2,
+    iwatch3,
+    iwatch4,
+
+    // shipping
+    aramex,
+    pep,
+    courier,
+}
